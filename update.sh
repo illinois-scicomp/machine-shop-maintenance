422836183a5c7dd3a9e53ef95327a29ffd4e969f
root <root@dunkel.cs.illinois.edu>
2016-08-15 19:32:20 -0500

    Lessons from setting up dunkel

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ set -x
 
 cd /shared/config
 
+mkdir -p /root/.ssh
 cp root_.ssh_authorized_keys /root/.ssh/authorized_keys
 
 cp etc_apt_apt.conf /etc/apt/apt.conf
@@ -12,15 +13,17 @@ cp etc_apt_sources.list /etc/apt/sources.list
 
 apt update
 apt install aptitude \
-  etckeeper htop sudo apt-listbugs apt-listchanges zsh \
+  etckeeper htop tmux sudo apt-listbugs apt-listchanges zsh \
   moreutils \
   unattended-upgrades \
   curl python3-yaml \
-  libnss-extrausers
+  libnss-extrausers \
+  mlocate
 
 cp etc_apt_apt.conf.d_02periodic /etc/apt/apt.conf.d/02periodic
 cp etc_apt_apt.conf.d_50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 
+mkdir -p /etc/scicomp-users
 if [[ "$(hostname)" != "porter" ]]; then
   cp etc_scicomp-users_update.sh /etc/scicomp-users/update.sh
   cp etc_scicomp-users_update-motd.sh /etc/scicomp-users/update-motd.sh
