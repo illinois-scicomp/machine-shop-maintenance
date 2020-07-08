218c478ebf679fe1a8cb622fb2bdc0daf2cf340a
root <root@porter.cs.illinois.edu>
2020-07-08 16:05:33 -0500

    Update Intel ICD

--- install-intel-icd.sh
+++ install-intel-icd.sh
@@ -5,7 +5,8 @@ set -x
 
 rm -f /etc/OpenCL/vendors/intel*.icd
 
-VERSION=oclcpuexp-2019.8.7.0.0725_rel
+#VERSION=oclcpuexp-2019.8.7.0.0725_rel
+VERSION=oclcpuexp-2020.10.6.0.4_rel
 
 OCLPATH="/opt/intel-$VERSION"
 
@@ -19,6 +20,6 @@ tar xvfz /shared/software/$VERSION.tar.gz
 chmod go+rX -R "$OCLPATH"
 
 echo "$OCLPATH/x64/libintelocl.so" > /etc/OpenCL/vendors/intel-$VERSION.icd
-rm -Rf "$OCLPATH/x64/libOpenCL.so*"
+rm -Rf $OCLPATH/x64/libOpenCL.so*
 
 echo "export LD_LIBRARY_PATH=$OCLPATH/x64:"'$LD_LIBRARY_PATH' > /opt/enable-intel-cl.sh
