8801a2b1f91a53a886ed8a0513f1931fab522031
Andreas Kloeckner <inform@tiker.net>
2021-06-27 13:10:51 -0500

    Use -f in docker cleanup

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -3,4 +3,4 @@
 # https://stackoverflow.com/a/35721111
 docker images --no-trunc --format '{{.ID}} {{.CreatedSince}}' \
     | grep ' months' | awk '{ print $1 }' \
-    | xargs --no-run-if-empty docker rmi
+    | xargs --no-run-if-empty docker rmi -f
