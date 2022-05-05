c65090b0073c66b60eab732b377cacb406f69a92
Andreas Kloeckner <inform@tiker.net>
2022-05-05 17:27:18 -0500

    Add reptyr for tmpi (tmux + mpi)

--- update.sh
+++ update.sh
@@ -57,7 +57,7 @@ with_echo apt install -y aptitude \
   rsync \
   etckeeper logrotate gnupg2 \
   htop iotop iftop tcpdump mtr ncdu rsync unison-all unison-all-gtk \
-  tmux screen tmate sudo apt-listbugs apt-listchanges \
+  tmux screen tmate sudo apt-listbugs apt-listchanges reptyr \
   zsh csh tcsh fish \
   moreutils \
   tig subversion mercurial git-lfs \
