a35eb6605b62d8a1fe0b6eb3b33ceebd5a6cc144
Andreas Kloeckner <inform@tiker.net>
2026-02-12 16:46:28 -0600

    UserDB update: ensure uv is available

--- etc_scicomp-users_update.sh
+++ etc_scicomp-users_update.sh
@@ -5,6 +5,8 @@ set -e
 cd /etc/scicomp-users
 
 if curl -s --fail --insecure -O https://porter:Cahk4voh@porter.cs.illinois.edu:1020/userdb.yml; then
+  pipx install uv
+  export PATH="$PATH:$HOME/.local/bin"
   /opt/extrausers-maint/extrausers-maint --db /etc/scicomp-users/userdb.yml update --with-shared
 else
   echo "*** FAILED TO UPDATE USER DATABASE"
