7c259c14749c8d9a5b7c1d8de6657dd720ba3d51
Andreas Kloeckner <inform@tiker.net>
2026-02-13 13:28:30 -0600

    userdb update: only install uv if not present

--- etc_scicomp-users_update.sh
+++ etc_scicomp-users_update.sh
@@ -5,8 +5,10 @@ set -e
 cd /etc/scicomp-users
 
 if curl -s --fail --insecure -O https://porter:Cahk4voh@porter.cs.illinois.edu:1020/userdb.yml; then
-  pipx install uv
   export PATH="$PATH:$HOME/.local/bin"
+  if ! command -v uv &> /dev/null ; then
+    pipx install uv
+  fi
   /opt/extrausers-maint/extrausers-maint --db /etc/scicomp-users/userdb.yml update --with-shared
 else
   echo "*** FAILED TO UPDATE USER DATABASE"
